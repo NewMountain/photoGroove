db2d59906f5f39dbab8bd93dd141e36a0bc969e4
Christopher Nyberg <christopher.nyberg@gmail.com>
2016-07-26 19:46:49 -0700

    Updated view function
    Just refactored a bit.

--- Main.elm
+++ Main.elm
@@ -1,19 +1,35 @@
 module Main exposing (..)
 
-import Html exposing (div, h1, img, text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+initialModel : List { url : String }
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+
+view : List { url : String } -> Html b
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+            (List.map viewThumbnail model)
         ]
 
 
+viewThumbnail thumbnail =
+    img [ src <| urlPrefix ++ thumbnail.url ] []
+
+
+main : Html a
 main =
-    view "no model yet"
+    view initialModel
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
